6de706720602c2e9b1c456d6afa0e45a10ed937e
Reuben Mathew <intelphysic@gmail.com>
2019-12-28 21:41:17 -0500

    redis docker image configured

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FlaskRedisTaskQueue
 Task Queue experimentation with docker
+
+
+## redis docker instance
+``` redis-cli -h $DB_PORT_6379_TCP_ADDR```
--- tskque.py
+++ tskque.py
@@ -1,5 +1,19 @@
 from flask import Flask, request
 import redis
 from rq import Queue
+import time
 
 app = Flask(__name__)
+
+r = redis.Redis()
+q = Queue(connection=r)
+
+def background_task(n):
+	delay = 2
+	print("Task Running...")
+	print(f'Simulating {delay} second delay')
+	time.sleep(delay)
+	print(len(n))
+	print("Task Complete")
+
+	return(len(n))
